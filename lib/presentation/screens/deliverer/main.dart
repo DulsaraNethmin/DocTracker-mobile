db3e8fecd54b8f43449fb07281b65c30633ee378
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-04-05 09:59:02 +0530

    reset

---
+++ lib/presentation/screens/deliverer/main.dart
@@ -0,0 +1,25 @@
+import 'package:doctracker/presentation/screens/deliverer/Welcome/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import '../../constants/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Test app',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
