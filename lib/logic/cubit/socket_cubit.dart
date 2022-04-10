03bb365045a60db87e33914f6532b58085a1c56a
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-04-10 20:18:14 +0530

    minor

--- lib/logic/cubit/socket_cubit.dart
+++ lib/logic/cubit/socket_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:doctracker/data/model/messageModel.dart';
 import 'package:doctracker/logic/cubit/user_cubit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,15 +9,16 @@ import 'package:socket_io_client/socket_io_client.dart' as IO;
 part 'socket_state.dart';
 
 class SocketCubit extends Cubit<SocketState> {
-  SocketCubit() : super(SocketState(socket: null));
+  SocketCubit() : super(SocketState(socket: null, messages: []));
 
   void connect(BuildContext context) {
     emit(SocketState(
         socket: IO.io(
             "https://intense-anchorage-44762.herokuapp.com/", <String, dynamic>{
-      "transports": ["websocket"],
-      "autoConnect": false
-    })));
+          "transports": ["websocket"],
+          "autoConnect": false
+        }),
+        messages: [...state.messages]));
     print("inside");
     state.socket!.connect();
     String id = context.read<UserCubit>().state.uuid;
@@ -25,6 +27,9 @@ class SocketCubit extends Cubit<SocketState> {
       print("connected");
       state.socket!.on('msg', (msg) {
         print(msg);
+        MessageModel message = MessageModel(
+            type: 'target', message: msg['message'], time: msg['time']);
+        state.messages.add(message);
         //setMessages("target", msg["message"], msg["time"]);
         //_scrollController.animateTo(_scrollController.position.maxScrollExtent,
         //    duration: Duration(microseconds: 300), curve: Curves.easeOut);
@@ -32,4 +37,27 @@ class SocketCubit extends Cubit<SocketState> {
     });
     print(state.socket!.connected);
   }
+
+  void sendMessage(String message, String time, String sender, String target) {
+    state.socket!.emit('msg',
+        {"message": message, "time": time, "sender": sender, "target": target});
+    //setMessages("sender", message, time);
+  }
+
+  void addMessages(MessageModel msg) {
+    state.messages.add(msg);
+  }
+
+  //   void sendMessage(String message, String time, String sender, String target) {
+  //   state.socket!.emit('msg',
+  //       {"message": message, "time": time, "sender": sender, "target": target});
+  //   setMessages("sender", message, time);
+  // }
+
+  // void setMessages(String type, String msg, String time) {
+  //   MessageModel message = MessageModel(type: type, message: msg, time: time);
+  //   setState(() {
+  //     messages.add(message);
+  //   });
+  // }
 }
