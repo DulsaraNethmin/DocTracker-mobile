76849d770e90d63d2f62ad4cee97a42b16bfee63
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-04-11 08:23:09 +0530

    user provider

--- lib/logic/cubit/user_cubit.dart
+++ lib/logic/cubit/user_cubit.dart
@@ -1,12 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-
+import '../../data/model/userModel.dart';
 part 'user_state.dart';
 
 class UserCubit extends Cubit<UserState> {
-  UserCubit() : super(UserState(username: "", uuid: ""));
+  UserCubit()
+      : super(UserState(
+            username: "",
+            uuid: "",
+            user: User(
+                uuid: "",
+                name: "",
+                email: "",
+                usenmae: "",
+                role: "",
+                branch: "")));
 
-  void setUser(String username, String uuid) {
-    emit(UserState(username: username, uuid: uuid));
+  void setUser(String username, String uuid, User user) {
+    emit(UserState(username: username, uuid: uuid, user: user));
+  }
+
+  bool getUser(String username, String password) {
+    UserRepo userRepo;
   }
 }
