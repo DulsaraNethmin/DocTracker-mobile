1b8f22041fe66a06a01cab2f895d063add6fac56
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-04-14 21:49:02 +0530

    document data get success

--- lib/data/repository/documentRepo.dart
+++ lib/data/repository/documentRepo.dart
@@ -12,14 +12,22 @@ class DocumentRepo {
   Future<List<Document>> getAllDocument(BuildContext context) async {
     final branch_id = context.read<UserCubit>().state.user.branchId;
     final branch = context.read<UserCubit>().state.user.branch;
-    final end_point = '/document/get/all?branch_id=${branch_id}';
-    final res = await _documentProvider.getAllDoc(end_point);
+    //final end_point = '/document/get/all?branch_id=${branch_id}';
+    final end_point = '/document/get/by/branch';
+    final res = await _documentProvider.getAllDoc(end_point, branch_id);
     print(res);
+    //print(res.data.length);
     List<Document> arr = [];
-    for (int i = 0; i < res.data.length; i++) {}
-    final jsonData = jsonEncode(res);
-    final jsondata2 = jsonDecode(jsonData);
-    arr = Document.fromJson(jsondata2);
+    for (int i = 0; i < res.data.length; i++) {
+      final jsonData = jsonEncode(res.data[i]);
+      final jsondata2 = jsonDecode(jsonData);
+      final doc_data = Document.fromJson(jsondata2);
+      arr.add(doc_data);
+    }
+    // final jsonData = jsonEncode(res);
+    // final jsondata2 = jsonDecode(jsonData);
+    // arr = Document.fromJson(jsondata2);
+    //print(arr[0].customerName);
     return arr;
   }
 }
