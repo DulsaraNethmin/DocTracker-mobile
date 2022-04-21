cbe980080a40871befc4e90935b883cd808a5cfc
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-04-21 23:24:11 +0530

    assign my_ip (aws EIP)

--- lib/presentation/constants/constants.dart
+++ lib/presentation/constants/constants.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
-const restAPI = 'http://3.110.165.97:8080';
+const restAPI = 'http://3.111.185.127:8080';
