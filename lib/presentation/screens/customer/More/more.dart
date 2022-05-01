8ab5088966ab9f0b537a75e599bd354cbfcb3a96
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-05-01 13:07:59 +0530

    add stack to profile pic

--- lib/presentation/screens/customer/More/more.dart
+++ lib/presentation/screens/customer/More/more.dart
@@ -50,11 +50,19 @@ class _MoreScreenState extends State<MoreScreen> {
     context.read<BotnavbarCubit>().onSelect(4);
     final bottom_sheet = GFBottomSheet(
         controller: _bottom_sheet_controller,
+        animationDuration: 250,
         maxContentHeight: 160,
-        stickyHeaderHeight: 100,
+        stickyHeaderHeight: 5,
+        elevation: 1,
+        stickyHeader: Container(
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 150, 129, 236),
+              boxShadow: [BoxShadow(color: Colors.black45, blurRadius: 0)]),
+          child: const GFListTile(),
+        ),
         contentBody: Container(
           height: 200,
-          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
           child: ListView(
             shrinkWrap: true,
             physics: const ScrollPhysics(),
@@ -91,31 +99,41 @@ class _MoreScreenState extends State<MoreScreen> {
         bottomSheet: bottom_sheet,
         body: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             Center(
-              child: InkWell(
-                onTap: () {
-                  onTapAvatar();
-                },
-                child: (image != null)
-                    ? ClipOval(
-                        child: Image.file(
-                        image!,
-                        width: 110,
-                        height: 110,
-                        fit: BoxFit.cover,
-                      ))
-                    : ClipOval(
-                        child: Image.asset(
-                        'assets/images/profile.png',
-                        width: 110,
-                        height: 110,
-                      )),
-              ),
+              child: Stack(children: [
+                InkWell(
+                  onTap: () {
+                    onTapAvatar();
+                  },
+                  child: (image != null)
+                      ? ClipOval(
+                          child: Image.file(
+                          image!,
+                          width: 110,
+                          height: 110,
+                          fit: BoxFit.cover,
+                        ))
+                      : ClipOval(
+                          child: Image.asset(
+                          'assets/images/profile.png',
+                          width: 110,
+                          height: 110,
+                        )),
+                ),
+                Positioned(
+                    bottom: 10,
+                    right: 5,
+                    child: const Icon(
+                      Icons.camera_alt_sharp,
+                      color: Color.fromARGB(255, 174, 171, 171),
+                      size: 30,
+                    ))
+              ]),
             ),
-            Divider(
+            const Divider(
               thickness: 2,
             )
           ],
