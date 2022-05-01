6308aafda8b1734ac49d1f50b6e4af87beb9533c
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-05-01 10:00:42 +0530

    bottom sheet - more

--- lib/presentation/screens/customer/More/more.dart
+++ lib/presentation/screens/customer/More/more.dart
@@ -1,30 +1,75 @@
 import 'package:doctracker/presentation/widgets/bottom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:image_picker/image_picker.dart';
 
-class MoreScreen extends StatelessWidget {
+class MoreScreen extends StatefulWidget {
   const MoreScreen({Key? key}) : super(key: key);
 
   @override
+  State<MoreScreen> createState() => _MoreScreenState();
+}
+
+class _MoreScreenState extends State<MoreScreen> {
+  final ImagePicker _picker = ImagePicker();
+  final GFBottomSheetController _bottom_sheet_controller =
+      GFBottomSheetController();
+  @override
   Widget build(BuildContext context) {
+    Future selectImage(BuildContext context) async {}
+
+    final bottom_sheet = GFBottomSheet(
+        controller: _bottom_sheet_controller,
+        maxContentHeight: 160,
+        stickyHeaderHeight: 100,
+        contentBody: Container(
+          height: 200,
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: ListView(
+            shrinkWrap: true,
+            physics: const ScrollPhysics(),
+            children: [
+              GFButton(
+                onPressed: () {},
+                text: "From Gallery",
+              ),
+              GFButton(
+                onPressed: () {},
+                text: "from camara",
+              ),
+              GFButton(
+                onPressed: () {},
+                text: "View Image",
+              ),
+            ],
+          ),
+        ));
+
+    void onTapAvatar() {
+      (_bottom_sheet_controller.isBottomSheetOpened)
+          ? _bottom_sheet_controller.hideBottomSheet()
+          : _bottom_sheet_controller.showBottomSheet();
+    }
+
     return SafeArea(
       child: Scaffold(
         bottomNavigationBar: MyBottomNavBar(),
+        bottomSheet: bottom_sheet,
         body: Column(
           children: [
             Padding(
               padding: const EdgeInsets.only(top: 30),
               child: Center(
-                child: GFAvatar(
-                  backgroundImage: AssetImage('assets/images/profile.png'),
-                  radius: 50,
+                child: InkWell(
+                  onTap: () {
+                    onTapAvatar();
+                  },
+                  child: GFAvatar(
+                    backgroundImage: AssetImage('assets/images/profile.png'),
+                    radius: 50,
+                  ),
                 ),
               ),
-            ),
-            GFButton(
-              onPressed: () {},
-              text: "Upload an Image",
-              type: GFButtonType.transparent,
             ),
             Divider(
               thickness: 2,
