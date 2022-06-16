8f5cdbf3e99f489fc8bc9fe099f8718dce68958a
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-06-16 20:53:02 +0530

    doc_request cubit

---
+++ lib/logic/cubit/doc_request_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'doc_request_state.dart';
+
+class DocRequestCubit extends Cubit<DocRequestState> {
+  DocRequestCubit() : super(DocRequestInitial());
+
+  void newRequest(String from, String doc_name) {
+    emit(DocRequest(doc_name: doc_name, from: from));
+  }
+
+  void toInitialState() {
+    emit(DocRequestInitial());
+  }
+}
