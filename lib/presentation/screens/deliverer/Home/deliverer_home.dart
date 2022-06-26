a2233fcf0367e5df50dc78a71a8d01e266d495ef
Chamodhya <chamodhyamanawathilake@gmail.com>
2022-06-26 23:41:13 +0530

    Deliverer Home

--- lib/presentation/screens/deliverer/Home/deliverer_home.dart
+++ lib/presentation/screens/deliverer/Home/deliverer_home.dart
@@ -1,114 +1,122 @@
+import 'package:doctracker/logic/cubit/user_cubit.dart';
 import 'package:doctracker/presentation/constants/constants.dart';
 import 'package:doctracker/presentation/widgets/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DelivererHome extends StatelessWidget {
   //const DelivererHome({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final user_state = context.read<UserCubit>().state;
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Deliverer Home'),
-        backgroundColor: kPrimaryColor,
-      ),
-      body:
-          // SafeArea(
-          //   child:
-          Column(
-        children: [
-          Container(
-            //padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Card(
-              margin: EdgeInsets.fromLTRB(10.0, 8.0, 10.0, 90.0),
-              color: kPrimaryColor,
-              child: Padding(
-                padding: EdgeInsets.all(40.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        //crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Jane Doe',
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white)),
-                          Text(
-                            'Id: xxxxxxxxx',
-                            style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white),
-                          )
-                        ],
-                      ),
+      // appBar: AppBar(
+      //   title: Text('Deliverer Home'),
+      //   backgroundColor: kPrimaryColor,
+      // ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.fromLTRB(10, 50, 10, 20),
+          child: Column(
+            children: <Widget>[
+              Card(
+                  margin: EdgeInsets.fromLTRB(10.0, 8.0, 10.0, 0.0),
+                  color: kPrimaryColor,
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Hi!",
+                                  style: TextStyle(
+                                      fontSize: 40,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white)),
+                              Text(
+                                (user_state is UserLogedin)
+                                    ? user_state.user.name
+                                    : "000",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    CircleAvatar(
-                      radius: 30,
-                      child: Image.asset('assets/images/profile.png'),
-                    )
-                  ],
-                ),
+                  )),
+              Image.asset(
+                "assets/images/deliverer.jpg",
               ),
-            ),
+              Row(
+                children: [
+                  Card(
+                      margin: EdgeInsets.fromLTRB(10.0, 8.0, 10.0, 0.0),
+                      color: kPrimaryLightColor,
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(50.0, 30.0, 50.0, 30.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text("My Jobs",
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black)),
+                                  Icon(
+                                    Icons.description_sharp,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                  Card(
+                      margin: EdgeInsets.fromLTRB(10.0, 8.0, 10.0, 0.0),
+                      color: kPrimaryLightColor,
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(60.0, 30.0, 60.0, 30.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text("About",
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black)),
+                                  Icon(
+                                    Icons.dvr,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ))
+                ],
+              )
+            ],
           ),
-          // Container(
-          //   child: GridView.count(
-          //     crossAxisCount: 2,
-          //     crossAxisSpacing: 8,
-          //     childAspectRatio: 1.0,
-          //     children: [
-          //       selectBoxes(
-          //         //image: 'assets/images/logo.png',
-          //         name: 'Tracking',
-          //       ),
-          //       selectBoxes(
-          //         //image: 'assets/images/logo.png',
-          //         name: 'Payments',
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          MaterialButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/more/del');
-            },
-            child: Text('More screen'),
-          )
-        ],
+        ),
       ),
-      //),
       bottomNavigationBar: BottomNavBar(),
     );
   }
-}
-
-Widget selectBoxes({required String name}) {
-  return Container(
-    decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5.0),
-        border: Border.all(color: Colors.black, width: 2)),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        // Container(
-        //   height: 8,
-        //   decoration:
-        //       BoxDecoration(image: DecorationImage(image: AssetImage(image))),
-        // ),
-        SizedBox(
-          height: 3,
-        ),
-        Text(
-          name,
-          style: TextStyle(fontSize: 5),
-        ),
-      ],
-    ),
-  );
 }
